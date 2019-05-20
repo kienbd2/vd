bb74c1567515ba480cb32bcf3a0f3d05a798e497
taolakienbui <buiduckien1999@gmail.com>
2019-05-20 22:07:45 +0700

    create a file

---
+++ vd1.js
@@ -0,0 +1,11 @@
+var express = require('express')
+var app = express()
+var port = 8080
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
+app.listen( port,function () {
+  console.log("Example app listening on port" + port)
+})
